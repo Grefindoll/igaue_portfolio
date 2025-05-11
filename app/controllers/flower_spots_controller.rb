a667fe4859efc22e2a6caf2a4553f7f6afb13e50
ray <torukonisuru@gmail.com>
2025-05-11 22:32:57 +0900

    [add] お花畑コントローラー関連の実装

--- app/controllers/flower_spots_controller.rb
+++ app/controllers/flower_spots_controller.rb
@@ -1,7 +1,48 @@
 class FlowerSpotsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_flower_spot, only: [:show, :edit]
+
+  def index
+    # これから実装するかも
+  end
+
+  def show
+    # before_action でセット済み
+  end
 
   def new
     @flower_spot = FlowerSpot.new
+  end
+
+  def create
+  @flower_spot = current_user.flower_spots.build(flower_spot_params)
+
+    if @flower_spot.save
+      redirect_to flower_spot_path(@flower_spot), notice: 'お花畑情報が正常に登録されました。'
+    else
+      flash.now[:alert] = 'お花畑情報の登録に失敗しました。入力内容を確認してください。'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def set_flower_spot
+    @flower_spot = FlowerSpot.find(params[:id])
+  end
+
+  def flower_spot_params
+    params.require(:flower_spot).permit(
+      :name,
+      :address,
+      { peak_season_months: [] }, # 配列としてパラメータを受け取ることを許可
+      :parking,
+      :fee_type,
+      :fee_detail,
+      :flower_type_details,
+      :official_url,
+      photos: [] # Active Storageで複数ファイルを受け取る場合
+    )
   end
 end
--- app/models/flower_spot.rb
+++ app/models/flower_spot.rb
@@ -4,4 +4,9 @@ class FlowerSpot < ApplicationRecord
 
   enum parking: { 不明: 0, 駐車場有り: 1, 駐車場無し: 2 }, _prefix: true
   enum fee_type: { 不明: 0, 無料: 1, 有料: 2, 一部有料: 3 }, _prefix: true
+
+  validates :name, presence: true, length: { maximum: 100}
+  validates :address, presence: true, length: { maximum: 255 }
+  validates :peak_season_months, presence: true
+  validates :photos, presence: true
 end
