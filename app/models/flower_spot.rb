d652aab4a1adf63eb9410c050ac95ceddf8d6238
ray <torukonisuru@gmail.com>
2025-05-15 19:29:23 +0900

    [update] モデルのenum を英語に修正。ただし、 ビューでは日本語になるようにja.ymlを修正

--- app/models/flower_spot.rb
+++ app/models/flower_spot.rb
@@ -2,8 +2,8 @@ class FlowerSpot < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
-  enum parking: { 不明: 0, 駐車場有り: 1, 駐車場無し: 2 }, _prefix: true
-  enum fee_type: { 不明: 0, 無料: 1, 有料: 2, 一部有料: 3 }, _prefix: true
+  enum parking: { unknown: 0, available: 1, not_available: 2 }, _prefix: true
+  enum fee_type: { unknown: 0, free: 1, paid: 2, partially_paid: 3 }, _prefix: true
 
   validates :name, presence: true, length: { maximum: 100}
   validates :address, presence: true, length: { maximum: 255 }
