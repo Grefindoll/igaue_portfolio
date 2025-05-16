73222a3ab791bee279c510c82ad9269313cc6f60
ray <torukonisuru@gmail.com>
2025-05-16 17:32:43 +0900

    [add] ユーザー情報が編集できるようにした。編集画面では、画像も登録できるようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
   has_many :flower_spots, dependent: :destroy
 
   validates :username, presence: true, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   post 'guest_sign_in', to: 'guest_sessions#create'
   root "home#index"
+  resources :users, only: [:show,:edit,:update]
   resources :flower_spots
 end
