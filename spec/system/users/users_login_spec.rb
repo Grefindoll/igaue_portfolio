63764132de7dcb5ef98af05e32c863068e7133cc
ray <torukonisuru@gmail.com>
2025-05-19 18:11:38 +0900

    [update] context から describe に変更。

--- spec/system/users/users_login_spec.rb
+++ spec/system/users/users_login_spec.rb
@@ -3,52 +3,50 @@ require 'rails_helper'
 RSpec.describe "UsersLogin", type: :system do
   let!(:user) { create(:user) }
 
-  describe "ログイン・ログアウト" do
-    context "ログイン" do
-      before do
-        visit root_path
-        click_link "ログイン"
-        fill_in "メールアドレス", with: user.email
-        fill_in "パスワード", with: user.password
-        click_button "ログイン"
-      end
+  describe "ログイン" do
+    before do
+      visit root_path
+      click_link "ログイン"
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password
+      click_button "ログイン"
+    end
 
-      it "ログインすると、ルートパスに遷移すること" do
-        expect(current_path).to eq root_path
-      end
+    it "ログインすると、ルートパスに遷移すること" do
+      expect(current_path).to eq root_path
+    end
 
-      it "ログイン成功時にフラッシュメッセージが表示されること" do
-        expect(page).to have_content "ログインしました。"
-      end
+    it "ログイン成功時にフラッシュメッセージが表示されること" do
+      expect(page).to have_content "ログインしました。"
+    end
 
-      it "ヘッダーにログアウトリンクが表示されること" do
-        expect(page).to have_link "ログアウト"
+    it "ヘッダーにログアウトリンクが表示されること" do
+      expect(page).to have_link "ログアウト"
+    end
+  end
+
+  describe "ログアウト" do
+    before do
+      visit new_user_session_path
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password
+      click_button "ログイン"
+      within "header" do
+        click_link "ログアウト"
       end
     end
 
-    context "ログアウト" do
-      before do
-        visit new_user_session_path
-        fill_in "メールアドレス", with: user.email
-        fill_in "パスワード", with: user.password
-        click_button "ログイン"
-        within "header" do
-          click_link "ログアウト"
-        end
-      end
+    it "ルートパスに遷移すること" do
+      expect(current_path).to eq root_path
+    end
 
-      it "ルートパスに遷移すること" do
-        expect(current_path).to eq root_path
-      end
+    it "ログアウト成功のフラッシュメッセージが表示されること" do
+      expect(page).to have_content "ログアウトしました。"
+    end
 
-      it "ログアウト成功のフラッシュメッセージが表示されること" do
-        expect(page).to have_content "ログアウトしました。"
-      end
-
-      it "ログインリンクが表示され、ログアウトリンクが消えること" do
-        expect(page).to have_link "ログイン"
-        expect(page).not_to have_link "ログアウト"
-      end
+    it "ログインリンクが表示され、ログアウトリンクが消えること" do
+      expect(page).to have_link "ログイン"
+      expect(page).not_to have_link "ログアウト"
     end
   end
 end
