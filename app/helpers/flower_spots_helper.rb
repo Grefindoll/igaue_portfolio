63521d0c8f5ec99a293950625ec372275f8828d3
ray <torukonisuru@gmail.com>
2025-05-20 00:12:32 +0900

    [update] ビューにtailwind css の追加

--- app/helpers/flower_spots_helper.rb
+++ app/helpers/flower_spots_helper.rb
@@ -1,2 +1,19 @@
 module FlowerSpotsHelper
+  def parking_options_for_select
+    options = FlowerSpot.parkings.keys.map do |key|
+      # I18n.t を使って日本語の表示名を取得します。
+      [I18n.t("activerecord.attributes.flower_spot.parkings.#{key}", default: key.to_s.humanize), key]
+    end
+    # 先頭に「指定なし」の選択肢を追加
+    [["指定なし", ""]] + options
+  end
+
+  def fee_type_options_for_select
+    options = FlowerSpot.fee_types.keys.map do |key|
+      # 同様に fee_type も修正します。
+      [I18n.t("activerecord.attributes.flower_spot.fee_types.#{key}", default: key.to_s.humanize), key]
+    end
+    # 先頭に「指定なし」の選択肢を追加
+    [["指定なし", ""]] + options
+  end
 end
