7682a5852cb830aebe0d6ca5dd7de278346b3332
ray <torukonisuru@gmail.com>
2025-05-20 19:07:40 +0900

    [update] 不要な箇所の削除。コメントの追加

--- app/helpers/flower_spots_helper.rb
+++ app/helpers/flower_spots_helper.rb
@@ -1,19 +1,15 @@
 module FlowerSpotsHelper
+  # フォーム用：選択肢を日本語に変換
+  # 配列の2番目の要素（key）が「実際にフォームから送られる値」。①コントローラー→②モデルで変換→③データベースに保存 という順番で変換されていく
   def parking_options_for_select
     options = FlowerSpot.parkings.keys.map do |key|
-      # I18n.t を使って日本語の表示名を取得します。
       [I18n.t("activerecord.attributes.flower_spot.parkings.#{key}", default: key.to_s.humanize), key]
     end
-    # 先頭に「指定なし」の選択肢を追加
-    [["指定なし", ""]] + options
   end
 
   def fee_type_options_for_select
     options = FlowerSpot.fee_types.keys.map do |key|
-      # 同様に fee_type も修正します。
       [I18n.t("activerecord.attributes.flower_spot.fee_types.#{key}", default: key.to_s.humanize), key]
     end
-    # 先頭に「指定なし」の選択肢を追加
-    [["指定なし", ""]] + options
   end
 end
